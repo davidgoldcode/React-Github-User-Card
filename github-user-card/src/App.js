08182df1725d4feb240f6feee3ffcaa5d2d45283
David Gold <davidgold@hey.com>
2020-09-01 15:32:32 -0400

    axios up and running

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
+import UserCard from './UserCard'
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      followers: [], 
+      error: '', 
+    };
+    console.log('constructor is running')
+  }
+
+  componentDidMount() {
+    console.log('cdm is running')
+    axios
+      .get('https://api.github.com/users/davidgoldcode/followers')
+      .then(res => {
+        debugger
+        this.setState({
+          followers: res.data,
+        })
+      })
+      .catch(err => {
+        console.log(err, 'you have an error');
+      })
+  }
+
+  render() {
+    console.log('rendering', this.state.followers)
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <UserCard />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ github-user-card/src/UserCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function UserCard(props) {
+
+
+    return (
+        <div>User</div>
+    );
+}
+
+export default UserCard;
